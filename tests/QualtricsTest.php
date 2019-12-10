8ab0f0cabf01be128cc1c059c8f32ac0a77fd34b
ssswang <ssswang@users.noreply.github.com>
2019-12-10 14:05:06 -0500

    Update QualtricsTest.php

--- tests/QualtricsTest.php
+++ tests/QualtricsTest.php
@@ -15,12 +15,4 @@ class QualtricsTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals($qt->getEndpoint() . '/', $qt->getClient()->uri);
   }
 
-  /**
-   * Test the version number.
-   */
-  public function testVersion() {
-    $qt = new Qualtrics();
-    $this->assertEquals($qt::VERSION, '1.0.0-alpha');
-    $this->assertEquals(json_decode(file_get_contents('composer.json'))->version, $qt::VERSION);
-  }
 }
